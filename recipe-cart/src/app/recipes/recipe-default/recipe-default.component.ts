b6b822a40921f72b30fab4b415eb7e1ea16fc5be
Jyasveer Gotta <jyasveer@gmail.com>
2018-01-05 11:51:37 +0530

    routing-started

---
+++ recipe-cart/src/app/recipes/recipe-default/recipe-default.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-default',
+  templateUrl: './recipe-default.component.html'
+})
+export class RecipeDefaultComponent {
+}
